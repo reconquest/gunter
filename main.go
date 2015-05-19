984e44e74e90bd3910b8839c50deb8b745743c61
Egor Kovetskiy <e.kovetskiy@office.ngs.ru>
2015-05-19 12:54:14 +0600

    initial version

---
+++ main.go
@@ -0,0 +1,189 @@
+package main
+
+import (
+	"fmt"
+	libtemplate "html/template"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+	"syscall"
+
+	"github.com/docopt/docopt-go"
+	"github.com/kovetskiy/scut"
+)
+
+const usage = `Gunter 1.0,
+
+Usage:
+    gunter [-c <config>] [-t <tpl>] [-d <dir>]
+    gunter [-c <config>] [-t <tpl>] -r
+
+Options:
+    -c <config>  Use specified config file.
+                 [default: /etc/gunter/config]
+	-t <tpl>     Set specified template directory.
+                 [default: /etc/gunter/templates/]
+    -d <dir>     Set specified directory as destination.
+	             [default: /]
+	-r           Generate temporary directory, print location and use it as
+                 destination directory.
+`
+
+func main() {
+	args, _ := docopt.Parse(usage, nil, true, "1.0", false)
+
+	var (
+		configFile     = args["-c"].(string)
+		templatesDir   = args["-t"].(string)
+		destinationDir = args["-d"].(string)
+		dryRun         = args["-r"].(bool)
+	)
+
+	config, err := scut.NewConfig(configFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	templates, err := getTemplates(templatesDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if dryRun {
+		destinationDir, err = getTempDir()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Printf("temporary directory: %s\n", destinationDir)
+	}
+
+	err = compileTemplates(templates, destinationDir, config.GetRoot())
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getTemplates(templatesDir string) ([]templateItem, error) {
+	storage, err := NewTemplateStorage(templatesDir)
+	if err != nil {
+		return nil, err
+	}
+
+	return storage.GetItems()
+}
+
+func compileTemplates(
+	templates []templateItem,
+	destinationDir string,
+	config map[string]interface{},
+) (err error) {
+	destinationDir = strings.TrimRight(destinationDir, "/") + "/"
+
+	for _, template := range templates {
+		switch {
+		case template.Mode().IsRegular():
+			err = compileTemplateFile(template, destinationDir, config)
+
+		case template.Mode()&os.ModeDir == os.ModeDir:
+			err = compileTemplateDir(template, destinationDir)
+
+		default:
+			err = fmt.Errorf(
+				"file '%s' has unsupported file type", template.RelativePath(),
+			)
+		}
+
+		if err != nil {
+			return err
+		}
+
+		err = applyTemplatePermissions(
+			destinationDir+template.RelativePath(),
+			template,
+		)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func compileTemplateDir(
+	template templateItem, destinationDir string,
+) error {
+	err := os.Mkdir(
+		destinationDir+template.RelativePath(), template.Mode(),
+	)
+
+	if err != nil && !os.IsExist(err) {
+		return err
+	}
+
+	err = applyTemplatePermissions(
+		destinationDir+template.RelativePath(),
+		template,
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func compileTemplateFile(
+	template templateItem,
+	destinationDir string,
+	config map[string]interface{},
+) error {
+	tpl, err := libtemplate.ParseFiles(template.FullPath())
+	if err != nil {
+		return err
+	}
+
+	compiledFile, err := os.OpenFile(
+		destinationDir+template.RelativePath(),
+		os.O_CREATE|os.O_WRONLY,
+		template.Mode(),
+	)
+	if err != nil {
+		return err
+	}
+
+	defer compiledFile.Close()
+
+	err = tpl.Execute(compiledFile, config)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getTempDir() (string, error) {
+	tempDir, err := ioutil.TempDir(os.TempDir(), "gunter")
+	if err != nil {
+		return "", err
+	}
+
+	tempDir = tempDir + "/"
+
+	return tempDir, nil
+}
+
+func applyTemplatePermissions(path string, template templateItem) error {
+	err := os.Chown(
+		path,
+		int(template.Sys().(*syscall.Stat_t).Uid),
+		int(template.Sys().(*syscall.Stat_t).Gid),
+	)
+	if err != nil {
+		return err
+	}
+
+	err = os.Chmod(path, template.Mode())
+
+	return err
+}
---
+++ templates.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type templateStorage struct {
+	dir    string
+	files  []templateItem
+	walked bool
+}
+
+type templateItem struct {
+	os.FileInfo
+	fullPath     string
+	relativePath string
+}
+
+func NewTemplateStorage(templatesDir string) (*templateStorage, error) {
+	templatesDir = strings.TrimRight(templatesDir, "/") + "/"
+
+	return &templateStorage{
+		dir:   templatesDir,
+		files: []templateItem{},
+	}, nil
+}
+
+func (storage *templateStorage) GetItems() ([]templateItem, error) {
+	if !storage.walked {
+		err := filepath.Walk(storage.dir, storage.walkDir)
+		if err != nil {
+			return []templateItem{}, err
+		}
+		storage.walked = true
+	}
+
+	return storage.files, nil
+}
+
+func (storage *templateStorage) walkDir(
+	fullPath string, info os.FileInfo, err error,
+) error {
+	if err != nil {
+		return err
+	}
+
+	relativePath := strings.TrimPrefix(fullPath, storage.dir)
+
+	// skip root dir
+	if relativePath == "" {
+		return nil
+	}
+
+	storage.add(info, fullPath, relativePath)
+
+	return nil
+}
+
+func (storage *templateStorage) add(
+	info os.FileInfo,
+	fullPath, relativePath string,
+) {
+	storage.files = append(
+		storage.files,
+		templateItem{
+			info,
+			fullPath,
+			relativePath,
+		},
+	)
+}
+
+// methods named by analogue with os.FileInfo Name()/Size()/Mode()
+func (item templateItem) RelativePath() string {
+	return item.relativePath
+}
+
+func (item templateItem) FullPath() string {
+	return item.fullPath
+}
